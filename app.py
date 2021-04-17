a507734ea91a0841f69ea65ce7fe230356c7abc3
Vaishali Guru <vaishaliguru12@gmail.com>
2021-04-18 00:14:38 +0530

    Modified App Description, Steps to use and Data Preprocessing Functions
    Modified the App Title and Description's positioning. Also, added the steps to use the app at the top and added the list of Data Preprocessing Functions

--- app.py
+++ app.py
@@ -8,15 +8,25 @@ logo = Image.open('logo.png')
 
 st.set_page_config(page_title = 'NLP-Data Preprocessing App', page_icon = logo, layout = 'wide')
 
-_, col1, _ = st.beta_columns(3)
+st.title('NLP-Data Preprocessing App')
 
-with col1:
-    st.title('NLP-Data Preprocessing App')
+st.markdown('''Get preprocessed and cleaned text data instantly with NLP-Data Preprocessing App! It is a NLP based Data Cleaning 
+App build on Python using Streamlit!''')
 
-c1 = st.beta_expander('App Description: ', expanded = True)
+e1 = st.beta_expander('Steps for using the App: ', expanded = True)
+with e1:
+    st.markdown('''1. Upload a CSV or Excel file to perform Data Cleaning\n2. Tick the checkboxes to select the text columns to preprocess
+    \n3. Click on **Preprocess Data** button to start data preprocessing\n4. See the preview of output dataset\n5. Click on **Download 
+    File** to download the output file (Provide the file extension as **.csv** while saving the output file)''')
+    st.info('Download option is only available if the output file size is less than **50MB**')
 
-with c1:
-    st.markdown('Get preprocessed and cleaned textual data instantly with NLP-Data Preprocessing App! It is a NLP based Data Cleaning App build on Python using Streamlit!')
+st.write('\n')
+
+e2 = st.beta_expander('Data Preprocessing Functions: ', expanded = False)
+with e2:
+    st.markdown('''1. Lowercase the text\n2. Remove common words/stopwords (e.g., as, in, the, etc.)\n3. Get all the words to root/base 
+    form\n4. Remove website URLs\n5. Remove email addresses\n6. Remove usernames or mentions(e.g., @xyz)\n7. Remove special characters 
+    and digits\n8. Expand contractions(e.g., i'll -> i will, i've -> i have, etc.)''')
 
 st.write('\n')
 
